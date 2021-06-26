b8da6af84605ab7f8d8a11186ee31b6cfa70adea
BaraaArnous <baraa.arnous@gmail.com>
2021-06-26 18:41:39 +0300

    cars

---
+++ js/app.js
@@ -0,0 +1,13 @@
+alert('hello');
+
+let qus = prompt('how are you');
+console.log(qus);
+
+let name = prompt('what is your name');
+console.log(name);
+
+let age = prompt ('how old are you');
+console.log(age);
+
+let adress = prompt ('where do you live');
+console.log(adress);
